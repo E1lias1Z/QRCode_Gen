20f8ae9e07d06f49dff1dcc61e90f141798f747c
E1lias1Z <ily.frolow2016@yandex.ru>
2021-08-04 21:11:53 +0400

    getting some changes of main prog

--- qr_gen.py
+++ qr_gen.py
@@ -4,7 +4,6 @@ from tkinter import messagebox
 
 import pyqrcode
 import png
-import random
 import string
 import re
 #-----------------------------
@@ -21,6 +20,9 @@ class Application:
         self.drawURLLabel()
         self.drawQRButton()
         self.drawSaveFolder()
+        self.drawNameLabel()
+        self.drawSafeQRButton()
+    
     #draws label for url-link
     def drawURLLabel(self):
         self.url = StringVar()
@@ -31,23 +33,44 @@ class Application:
         self.save_folder = StringVar()
         Label(text="Путь сохранения:", font="Arial 12").grid(row=6, column=0, padx=20)
         folder_Entry = Entry(self.mainWin, textvariable=self.save_folder, width=60).grid(row=6, column=1)
+
+    def drawNameLabel(self):
+        self.filename = StringVar()
+        Label(text="Имя файла:", font="Arial 12").grid(row=7, column=0, padx=20, pady=20)
+        name_entry = Entry(self.mainWin, textvariable=self.filename, width=60).grid(row=7, column=1)
+
     #generate QRCode and saves to png-file
-    def genQRCode(self):
+    def saveQRCode(self):
         # try:
-        url_get = self.url.get(); save_fol_get = self.save_folder.get()
+        save_fol_get = self.save_folder.get()
         folder_correct = re.sub(r'\\', r'\\\\', save_fol_get)
-        if(url_get):
-            qr_code = pyqrcode.create(url_get); nameFile = "QR_code_" + random.choice(string.ascii_letters) + ".png"
-            folder = folder_correct + '\\' + nameFile
-            qr_code.png(folder, scale = 10)
-            Label(text="QR-код сгенерирован успешно!", font="Arial 14 bold", fg="#32CD32").grid(row=7, column=1, pady=20)
+        nameFile = self.filename.get() + ".png"
+        folder = folder_correct + '\\' + nameFile
+        try:
+            if(self.url_get):
+                self.qr_code.png(folder, scale = 6)
+                Label(text="QR-код сохранен успешно!", font="Arial 14 bold", fg="#32CD32").grid(row=9, column=1, pady=20)
+        except:
+            messagebox.showerror("Ошибка!", "QR-код не сгенерирован успешно!")
+    #showing the generated code on main window
+    def ShowCode(self):
+        global photo
+        self.url_get = self.url.get()
+        if(self.url_get):
+            self.qr_code = pyqrcode.create(self.url_get)
+            qrImage = self.qr_code.xbm(scale=6)
+            photo = BitmapImage(data=qrImage)
+            notificationLabel = Label(self.mainWin)
+            notificationLabel.grid(row=8, column=1, padx=40)
+            notificationLabel.config(image = photo)
         else:
             messagebox.showerror("Ошибка!", "URL не был введен или введен неверно! Повторите ввод!")
-        # except exceptions.RegexMatchError:
-        #     messagebox.showerror("Ошибка!", "Введен неверный URL! Повторите ввод!")
     #draws button for make QRCode command
     def drawQRButton(self):
-        Button(self.mainWin, text='Сгенерировать QR-код', command=self.genQRCode).grid(row=7, column=0, padx=20, pady=20)
+        Button(self.mainWin, text='Сгенерировать QR-код', command=self.ShowCode, font="Arial 12").grid(row=6, column=2, padx=20, pady=20)
+    #draws button for saving to PNG-file
+    def drawSafeQRButton(self):
+        Button(self.mainWin, text='Сохранить в PNG', font="Arial 12", command=self.saveQRCode).grid(row=7, column=2, padx=20, pady=20)
     #running application
     def run(self):
         self.widgetsApp()
@@ -55,8 +78,9 @@ class Application:
 
 #--------------------------------------------------------------------------------------------
 
-app = Application(600, 200)
+app = Application(800, 580)
 app.run()
+
 #-------------------------------------------------------------------------------------------- 
 
 
